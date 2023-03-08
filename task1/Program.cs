f9e3c646c0dc1e62b32a574f36556968df2a6dcc
«Alleria» <lesok.22.12.95@mail.ru>
2023-03-09 04:46:03 +0500

    homework

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] matrix = new double[3, 4];
+
+void PrintArray(double[,] matr)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			Console.Write($"{matr[i, j]} ");
+		}
+		Console.WriteLine();
+	}
+}
+
+void FillArray(double[,] matr)
+{
+	for (int i = 0; i < matr.GetLength(0); i++)
+	{
+		for (int j = 0; j < matr.GetLength(1); j++)
+		{
+			matr[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+		}
+	}
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = new int[3, 4];
+void FillArray(int[,] array) //Заполнение массива рандомными числами
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			array[i, j] = new Random().Next(0, 11);
+		}
+	}
+}
+
+
+void PrintArray(int[,] array) // Вывод массива в терминал
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(array[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+	Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+	double sum = 0;
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		sum = Math.Round(((sum + array[i, j]) / 3), 1);
+
+	}
+	Console.Write($"{sum} ");
+}
+
